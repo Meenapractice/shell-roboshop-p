070c6b781549e2e5954b058c745b8154373056c1
meena_test <abc@gmail.com>
2026-04-20 10:20:21 +0530

    added code

---
+++ mongodb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/home/ec2-user/shell-roboshop-p/logs"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R='\e[31m'
+Y='\e[32m'
+G='\e[33m'
+N='\e[0m'
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]; then
+  echo -e "$R Please run the script with root user access $N" | tee -a $LOGS_FILE
+  exit 1
+fi
+
+VALIDATE() {
+  if [ $1 -ne 0 ]; then
+    echo -e "$2...$R FAILURE $N" | tee -a $LOGS_FILE
+    exit 1
+  else
+    echo -e "$2..$G SUCCESS $N" | tee -a $LOGS_FILE
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongo.repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "Installing mongodb"
+
+systemctl enable mongod 
+systemctl start mongod 
+VALIDATE $? "Enable and starting mongod"
+
+sed -i "%s/127.0.0.1/0.0.0.0" /etc/mongod.conf
+VALIDATE $? "Changing private address in conf file"
+
+systemctl restart mongod
+VALIDATE $? "Restarting mongod"
